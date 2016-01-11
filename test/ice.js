d72de70d4e6edfa5d9b1f95c413cd2f099af3dc6
Nick Desaulniers <ndesaulniers@mozilla.com>
2016-01-11 15:51:18 -0800

    move a lot of debugging to lib/debug.js

--- test/ice.js
+++ test/ice.js
@@ -1,5 +1,12 @@
 var IceAgent = require('../lib/ice');
+var RTCIceCandidate = require('../lib/RTCIceCandidate');
 var test = require('tape');
+
+var iceServers = [
+  {
+    urls: 'stun:stun.l.google.com:19302',
+  }
+];
 
 test('constructor should throw for missing ICE server list', function (t) {
   t.plan(1);
@@ -8,3 +15,22 @@ test('constructor should throw for missing ICE server list', function (t) {
     var agent = new IceAgent;
   });
 });
+
+test('gatherHostCandidates', function (t) {
+  t.plan(1);
+
+  var agent = new IceAgent({
+    iceServers: iceServers,
+  });
+  agent.gatherHostCandidates().then(function (candidates) {
+    candidates.filter(function (candidate) {
+      return candidate instanceof RTCIceCandidate;
+    });
+    agent.shutdown();
+    t.ok(candidates.length > 0, 'had some host candidates');
+  }).catch(function (e) {
+    agent.shutdown();
+    t.fail(e);
+  });
+});
+
